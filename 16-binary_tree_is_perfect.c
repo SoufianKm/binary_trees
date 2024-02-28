5f13950604f3b8ec6dad3d541c624623bf4a9634
SoufianKm <soufian.kamma@gmail.com>
2024-02-28 06:27:44 -0800

    Task 16 - is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 0 if it is not perfect, otherwise a pair number
+ */
+int is_perfect(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree->left && !tree->right)
+		return (1);
+	else if (tree->left && tree->right)
+	{
+		left = 1 + is_perfect(tree->left);
+		right = 1 + is_perfect(tree->right);
+		if (right == left && right != 0 && left != 0)
+			return (right + left);
+	}
+
+	return (0);
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  *
@@ -8,20 +31,8 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-	else if (tree->left && tree->right)
-	{
-		left = 1 + binary_tree_is_perfect(tree->left);
-		right = 1 + binary_tree_is_perfect(tree->right);
-		if (right == left && right != 0 && left != 0)
-			return (1);
-	}
-
-	return (0);
+	return (is_perfect(tree->right));
 }
